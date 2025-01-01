cf118b0410199df52ae73fcff822afcbb0d9045b
Nathan Hüsken <nathan@huesken>
2025-01-01 14:03:32 +0100

    chore: add function to download/create resnet model for tests

---
+++ export_resnet.py
@@ -0,0 +1,20 @@
+# copied from https://github.com/elixir-nx/ortex/blob/main/python/export_resnet.py
+import torch
+import torchvision.models as models
+
+model = models.resnet50(pretrained=True)
+
+model.eval()
+onnx_input = torch.randn(1, 3, 224, 224)
+
+torch.onnx.export(
+    model,
+    onnx_input,
+    "models/resnet50.onnx",
+    verbose=False,
+    input_names=["input"],
+    output_names=["output"],
+    dynamic_axes={"input": {0: "batch_size"}, "output": {0: "batch_size"}},
+    export_params=True,
+    opset_version=19,
+)
